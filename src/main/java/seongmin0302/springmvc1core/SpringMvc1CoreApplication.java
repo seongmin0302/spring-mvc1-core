3d9edcd8ac15810a8e51be629be2c658fa729482
seongmin0302 <seongmin0302@naver.com>
2026-04-29 10:23:26 +0000

    프로젝트 생성

---
+++ src/main/java/seongmin0302/springmvc1core/SpringMvc1CoreApplication.java
@@ -0,0 +1,13 @@
+package seongmin0302.springmvc1core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc1CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc1CoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seongmin0302/springmvc1core/SpringMvc1CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package seongmin0302.springmvc1core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
