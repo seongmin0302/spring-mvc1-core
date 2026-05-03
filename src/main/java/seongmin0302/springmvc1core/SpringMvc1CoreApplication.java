bbdc71a11fd05bb28a6ab88fc93a19d9be1bcdc1
seongmin0302 <seongmin0302@naver.com>
2026-05-03 10:33:15 +0000

    servlet으로 get 쿼리파라미터 받아오기

--- src/main/java/seongmin0302/springmvc1core/SpringMvc1CoreApplication.java
+++ src/main/java/seongmin0302/springmvc1core/SpringMvc1CoreApplication.java
@@ -2,7 +2,9 @@ package seongmin0302.springmvc1core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringMvc1CoreApplication {
 
---
+++ src/main/java/seongmin0302/springmvc1core/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package seongmin0302.springmvc1core.basic;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service()");
+
+        System.out.println(request);
+        System.out.println(response);
+
+        String username = request.getParameter("username");
+        System.out.println("username: " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+    
+}
