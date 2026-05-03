12455d6346fc78ae73904c0751d61847f01a89bf
seongmin0302 <seongmin0302@naver.com>
2026-05-03 11:28:07 +0000

    서블릿으로 json데이터 받기

---
+++ src/main/java/seongmin0302/springmvc1core/basic/HelloData.java
@@ -0,0 +1,11 @@
+package seongmin0302.springmvc1core.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
