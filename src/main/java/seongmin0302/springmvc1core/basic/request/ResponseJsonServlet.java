977d4a95080a8243dc8b2097fed903b30d0ec5f1
seongmin0302 <seongmin0302@naver.com>
2026-05-03 13:57:58 +0000

    json 응답으로 내보내기

--- src/main/java/seongmin0302/springmvc1core/basic/request/ResponseJsonServlet.java
+++ src/main/java/seongmin0302/springmvc1core/basic/request/ResponseJsonServlet.java
@@ -1,5 +1,34 @@
 package seongmin0302.springmvc1core.basic.request;
 
-public class ResponseJsonServlet {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import seongmin0302.springmvc1core.basic.HelloData;
+import tools.jackson.databind.ObjectMapper;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("content-type", "application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+
+        String result = objectMapper.writeValueAsString(data);
+        response.getWriter().write(result);
+
+    }
+    
 
 }
